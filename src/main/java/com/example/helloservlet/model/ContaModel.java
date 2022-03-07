99466462197ae2ec874abe891b55dc7e007f2a2d
PrenitaVulpo <guilhermepdl97@protonmail.com>
2022-03-06 23:14:11 -0300

    Finish get ContaModel

--- src/main/java/com/example/helloservlet/model/ContaModel.java
+++ src/main/java/com/example/helloservlet/model/ContaModel.java
@@ -1,4 +1,29 @@
 package com.example.helloservlet.model;
 
+import com.example.helloservlet.Cliente;
+
 public class ContaModel {
+    private Double saldo;
+    private Cliente cliente;
+
+    public ContaModel(Double saldo, Cliente cliente){
+        this.saldo = saldo;
+        this.cliente = cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
 }
